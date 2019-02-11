50e91a1db748b0a39f57f5bc78004be2d71a124d
Mohak666 <mohak.rathore06@gmail.com>
2019-02-11 18:44:13 +0530

    finished app-promises.js

--- weatherApp/app-promises.js
+++ weatherApp/app-promises.js
@@ -21,7 +21,7 @@ const argv = yargs
 var encodedAddress = encodeURIComponent(argv.address);
 
 var requestURL = 'http://www.mapquestapi.com/geocoding/v1/address?key=ciZURLPwb13FU5Hp4FI8U4HNPsl79PGV&location='+encodedAddress;
-
+// console.log(requestURL)
 axios.get(requestURL).then((response) => {
   // console.log(response.data);
   if(!response.data){
@@ -30,20 +30,26 @@ axios.get(requestURL).then((response) => {
   else if(response.data.info.statuscode === 0 && response.data){
     var lat =  response.data.results[0].locations[0].latLng.lat;
     var lng = response.data.results[0].locations[0].latLng.lng;
-    address: response.data.results[0].locations[0].adminArea5 +', '+ response.data.results[0].locations[0].adminArea3 +' '+ response.data.results[0].locations[0].postalCode +', '+ body.results[0].locations[0].adminArea1
-    url: 'https://api.darksky.net/forecast/7975ab8100ed76ec2e4c11648d0fad56/'+lat+','+lng,
-    console.log(lat,lng);
-  }
+    var address = response.data.results[0].locations[0].adminArea5 +', '+ response.data.results[0].locations[0].adminArea3 +' '+ response.data.results[0].locations[0].postalCode +', '+ response.data.results[0].locations[0].adminArea1
+    weatherURL =  'https://api.darksky.net/forecast/7975ab8100ed76ec2e4c11648d0fad56/'+lat+','+lng;
+    // console.log(weatherURL);
+    console.log(lat,lng,address);
+    return axios.get(weatherURL);
+ }
   else {
     throw new Error('unable to find address');
   }
 
-
+}).then((response) => {// runs after successful return from weatherURL
+  temperture = response.data.currently.temperature;
+  apparentTemperture = response.data.currently.apparentTemperature;
+  console.log(`The temperature is ${temperture}, and it feels like ${apparentTemperture} `);
 }).catch((e) => {
   if(e.code === 'ENOTFOUND'){
     console.log('unable to connect to API servers');
   }
   else if(e.message){
+        // console.log('here')
         console.log(e.message);
   }
 
