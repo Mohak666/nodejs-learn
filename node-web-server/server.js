af15b74b81cb3626b1a8126a5b7a24dac89193af
Mohak666 <mohak.rathore06@gmail.com>
2019-02-14 20:39:10 +0530

    defined a bit of middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,16 +1,42 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')// __dirname storepath to project directory
 app.set('view engine','hbs');
-app.use(express.static(__dirname + '/public'));// __dirname storepath to project directory
+
+// middleware
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs');
+// });
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.originalUrl}`
+  fs.appendFile('server.log',log + "\n", (err) => {
+    if(err){
+      console.log('Unable to append to server.log');
+    }
+  });
+  console.log(log)
+  next();// next() runs when middleware function is done,
+     // if next() is not called , any of the routes(/*) will never fire
+});
+
+app.use(express.static(__dirname + '/public' ));
+
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text) => {
+  return text.toUpperCase();
+});
 
 app.get('/',(req,res) => {
   // res.send('<h1>Hello Express!</h1>');
   res.render('home.hbs',{
     pageTitle: 'Home Page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Welcome to home page',
   });
 });
@@ -18,7 +44,6 @@ app.get('/',(req,res) => {
 app.get('/about',(req,res) => {
     res.render('about.hbs',{
       pageTitle: 'About Page',
-      currentYear: new Date().getFullYear(),
     });
 });
 
