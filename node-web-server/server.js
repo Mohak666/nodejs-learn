638efa35465b3573bb034444d805c99cf780277d
Mohak666 <mohak.rathore06@gmail.com>
2019-02-22 19:19:16 +0530

    added notes/comments in server.js

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -7,13 +7,16 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials')// __dirname storepath to project directory
 app.set('view engine','hbs');
 
-// middleware
+// middleware --- app.use() is used to register middleware
+// *** middleware executes in order of definition ***
+
 // app.use((req,res,next) => {
 //   res.render('maintenance.hbs');
 // });
+
 app.use((req,res,next) => {
   var now = new Date().toString();
-  var log = `${now}: ${req.originalUrl}`
+  var log = `${now}: ${req.method} ${req.originalUrl}`
   fs.appendFile('server.log',log + "\n", (err) => {
     if(err){
       console.log('Unable to append to server.log');
@@ -25,6 +28,7 @@ app.use((req,res,next) => {
 });
 
 app.use(express.static(__dirname + '/public' ));
+
 
 hbs.registerHelper('getCurrentYear',() => {
   return new Date().getFullYear();
@@ -49,11 +53,11 @@ app.get('/about',(req,res) => {
 
 app.get('/bad',(req,res) => {
     res.send({
-      errorMessage:'Bad Request'
+      errorMessage:'unable to handle request'
     });
 });
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
-// 
+//
